d8894fa3ba09fe9765b725ee8f32c4a185ec143e
vulasau <valiantsin.ulasau@gmail.com>
2022-12-14 17:25:00 +0100

    Update generate-ssl.sh

--- generate-ssl.sh
+++ generate-ssl.sh
@@ -10,13 +10,13 @@ cd ssl
 if [ -z "$1" ]
 then
   echo "Generating ssl certificate for localhost"
-  mkcert -key-file unite-key.pem -cert-file unite-cert.pem localhost 127.0.0.1 ::1
+  mkcert -key-file upsi-key.pem -cert-file upsi-cert.pem localhost 127.0.0.1 ::1
 else
   echo "Generating ssl certificate for '$1'"
-  mkcert -key-file unite-key.pem -cert-file unite-cert.pem $1 localhost 127.0.0.1 ::1
+  mkcert -key-file upsi-key.pem -cert-file upsi-cert.pem $1 localhost 127.0.0.1 ::1
 fi
 
-chmod 600 unite-key.pem
-chmod 600 unite-cert.pem 
+chmod 600 upsi-key.pem
+chmod 600 upsi-cert.pem 
 
 cd ..
