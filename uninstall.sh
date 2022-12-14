96cad4a6a44855f49aad99fc65286d89ddb72676
DGitRich <106763957+DGitRich@users.noreply.github.com>
2022-12-14 17:42:19 +0100

    Update uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,6 @@ tput setaf 6; echo "# Removing upsi containers"; tput sgr0
 docker rm -f upsi
 echo ""
 
-tput setaf 6; echo "# Cleaning up unused images"; tput sgr0
-docker image prune -f
-
 tput setaf 6; echo "# Removing upsi images"; tput sgr0
 docker image rm -f upsi
 echo ""
