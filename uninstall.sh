31ac6d1372b2231590b5b94b339d1b4040debb9e
vulasau <valiantsin.ulasau@gmail.com>
2022-12-19 16:37:32 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 tput setaf 6; echo "# Removing upsi containers"; tput sgr0
-docker rm -f upsi
+docker service rm unite_upsi
+echo ""
+
+tput setaf 6; echo "# Removing upsi images"; tput sgr0
+docker image rm ghcr.io/dkfz-unite/upsi
 echo ""
